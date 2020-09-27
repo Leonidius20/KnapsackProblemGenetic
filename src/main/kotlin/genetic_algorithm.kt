270c9f03e647545a6456fad057be92642fa2d1b8
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2020-09-27 15:34:35 +0300

    implemented the algorithm, but resulting weight is bigger than capacity

---
+++ src/main/kotlin/genetic_algorithm.kt
@@ -0,0 +1,78 @@
+private typealias Solution = Array<Boolean>
+private typealias Validator = (Solution) -> Boolean
+
+fun genetic(solutionLength: Int,
+            validator: Validator,
+            fitness: (Solution) -> Int,
+            crossover: (Solution, Solution) -> Solution,
+            mutation: (Solution) -> Solution,
+            localImprovement: (Solution) -> Solution,
+            maxIterations: Int): Solution {
+
+    val population = generateRandomSolutions(solutionLength)
+
+    var iterationCounter = 0
+    while (iterationCounter <= maxIterations) {
+        iterationCounter++
+
+        // finding best and worst solutions
+        val (bestIndex, worstIndex)
+                = evaluateSolutions(population, fitness)
+
+        // selecting parents for a new solution
+        val parent1 = population[bestIndex]
+
+        var parent2 = population.random()
+        while (parent2.contentEquals(parent1)) {
+            parent2 = population.random() // potential endless loop?
+        }
+
+        // creating a new solution
+        val newSolution = localImprovement(mutation(crossover(parent1, parent2)))
+
+        // validating a new solution
+        if (!validator(newSolution)) continue
+
+        // replacing the worst solution with the new one
+        population[worstIndex] = newSolution
+    }
+
+    return population[evaluateSolutions(population, fitness).bestIndex]
+}
+
+/**
+ * Generates random solutions, each of which has only one boolean set to 1
+ * solution length == number of solutions in this population
+ */
+private fun generateRandomSolutions(solutionLength: Int): Array<Solution> {
+    var index = 0
+    return Array(solutionLength) {
+        Solution(solutionLength) { it == index++ }
+    }
+}
+
+private data class BestAndWorstSolutions(val bestIndex: Int, val worstIndex: Int)
+
+private fun evaluateSolutions(population: Array<Solution>, fitness: (Solution) -> Int): BestAndWorstSolutions {
+    var worstIndex = 0
+    var worstFitness: Int = fitness(population[0])
+
+    var bestIndex = 0
+    var bestFitness: Int = fitness(population[0])
+
+    population.forEachIndexed { index, solution ->
+        val solutionFitness = fitness(solution)
+
+        if (solutionFitness < worstFitness) {
+            worstIndex = index
+            worstFitness = solutionFitness
+        }
+
+        if (solutionFitness > bestFitness) {
+            bestIndex = index
+            bestFitness = solutionFitness
+        }
+    }
+
+    return BestAndWorstSolutions(bestIndex, worstIndex)
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,64 @@
+import kotlin.random.Random
+import kotlin.random.nextInt
 
+const val CAPACITY = 250
+const val NUMBER_OF_ITEMS = 100
 
-fun main(args: Array<String>) {
-    println("Hello World!")
+data class Item(val cost: Int, val weight: Int)
+
+val items = Array(NUMBER_OF_ITEMS) {
+    Item(Random.nextInt(2, 31), Random.nextInt(1, 26))
+}
+
+fun main() {
+    val validator = { solution: Array<Boolean> ->
+        var sum = 0
+        solution.forEachIndexed { index, value ->
+            if (value) sum += items[index].weight
+        }
+        sum <= CAPACITY
+    }
+
+    val fitnessFunction = { solution: Array<Boolean> ->
+        var sum = 0
+        solution.forEachIndexed { index, value ->
+            if (value) sum += items[index].cost
+        }
+        sum
+    }
+
+    val crossover = { parent1: Array<Boolean>, parent2: Array<Boolean> ->
+        val firstQuarter = parent1.slice(0 until 25)
+        val secondQuarter = parent2.slice(25 until 50)
+        val thirdQuarter = parent1.slice(50 until 75)
+        val fourthQuarter = parent2.slice(75 until 100)
+
+        firstQuarter.plus(secondQuarter).plus(thirdQuarter).plus(fourthQuarter).toTypedArray()
+    }
+
+    val mutation = { solution: Array<Boolean> ->
+        if (Random.nextInt(0..100) <= 5) { // 5% probability
+            val index1 = Random.nextInt(0 until NUMBER_OF_ITEMS)
+            val index2 = Random.nextInt(0 until NUMBER_OF_ITEMS)
+            val temp = solution[index1]
+            solution[index1] = solution[index2]
+            solution[index2] = temp
+        }
+        solution
+    }
+
+    val localImprovement = { solution: Array<Boolean> ->
+        solution // TODO
+    }
+
+    val result = genetic(solutionLength = NUMBER_OF_ITEMS, validator, fitnessFunction,
+            crossover, mutation, localImprovement, maxIterations = 1000)
+
+    println("Cost: ${fitnessFunction(result)}")
+
+    var weight = 0
+    result.forEachIndexed { index, value ->
+        if (value) weight += items[index].weight
+    }
+    println("Weight: $weight")
 }
